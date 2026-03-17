69c8563e448f2119c072e921598327fa09e5aa16
cofon <15515579739@163.com>
2026-03-17 20:24:08 +0800

    测试hello失败，修改代码继续测试

--- edge-functions/api/hello.js
+++ edge-functions/api/hello.js
@@ -1,3 +1,15 @@
-export default async function () {
+// 错误
+// export default async function () {
+//   return new Response("Hello from EdgeOne Pages!");
+// }
+
+
+// 注意：导出名称必须是 onRequest (匹配所有方法) 或 onRequestGet (仅限 GET)
+export default function onRequest() {
   return new Response("Hello from EdgeOne Pages!");
 }
+
+// async
+// export const onRequest = async (context) => {
+//   return new Response("Hello from EdgeOne Pages!");
+// };
